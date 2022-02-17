3bf1b6199c743f830c6bfe22d18134c72dfd79b3
Pau Ibáñez <pauibanez2001@gmail.com>
2022-02-17 22:02:46 +0100

    added logic to connect to the database

--- db/index.js
+++ db/index.js
@@ -1,1 +1,18 @@
+const debug = require("debug")("app:db:root");
+const chalk = require("chalk");
+const mongoose = require("mongoose");
 
+const connectToDatabase = (connectionString) =>
+  new Promise((resolve, reject) => {
+    debug(chalk.whiteBright("Connecting to database..."));
+    mongoose.connect(connectionString, (error) => {
+      if (error) {
+        reject(new Error(`Couldn't connect to the database: ${error.message}`));
+        return;
+      }
+      debug(chalk.yellowBright("Connection to database SUCESSFULL"));
+      resolve();
+    });
+  });
+
+module.exports = connectToDatabase;
