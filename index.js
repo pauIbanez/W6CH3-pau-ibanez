869d6b1049afef8f105db50e8d9e9454bccbfb5c
Pau Ibáñez <pauibanez2001@gmail.com>
2022-02-17 21:02:14 +0100

    added error catching

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 require("dotenv").config();
+const debug = require("debug")("app:root");
+const chalk = require("chalk");
 
 const initializeServer = require("./server");
 const getProgramGlobals = require("./utils/initUtils");
 
 (async () => {
-  const globals = await getProgramGlobals();
+  try {
+    const globals = await getProgramGlobals();
 
-  await initializeServer(globals);
+    await initializeServer(globals);
+  } catch (error) {
+    debug(
+      chalk.redBright(`There was an error on the server: ${error.message}`)
+    );
+  }
 })();
