ab82c7d1470bf785635ab26799156f3fa18f1a42
Pau Ibáñez <pauibanez2001@gmail.com>
2022-02-17 22:11:22 +0100

    added dev conection string

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const debug = require("debug")("app:root");
 const chalk = require("chalk");
+const connectToDatabase = require("./db");
 
 const initializeServer = require("./server");
 const getProgramGlobals = require("./utils/initUtils");
@@ -9,7 +10,12 @@ const getProgramGlobals = require("./utils/initUtils");
   try {
     const globals = await getProgramGlobals();
 
+    const connectionString = globals.devDatabase
+      ? process.env.DEV_CONN_STRING
+      : process.env.CONN_STRING;
+
     await initializeServer(globals);
+    await connectToDatabase(connectionString);
   } catch (error) {
     debug(
       chalk.redBright(`There was an error on the server: ${error.message}`)
