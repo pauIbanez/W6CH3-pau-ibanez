691d7d3824f0eeab4c230233dd50cd342b716bf2
Pau Ibáñez <pauibanez2001@gmail.com>
2022-02-17 23:13:57 +0100

    added put and post handlers

--- server/routers/thingsRouter.js
+++ server/routers/thingsRouter.js
@@ -47,4 +47,28 @@ router.delete("/:id", async (req, res, next) => {
   }
 });
 
+router.post("/", async (req, res, next) => {
+  try {
+    const newThing = await Thing.create(req.body);
+    res.json(newThing);
+  } catch (error) {
+    error.type = errorTypes.badRequest;
+    error.info =
+      "This is likely to be because of you are missing the text propperty";
+    next(error);
+  }
+});
+
+router.put("/", async (req, res, next) => {
+  try {
+    // eslint-disable-next-line no-underscore-dangle
+    await Thing.replaceOne({ _id: req.body._id }, { ...req.body });
+    res.json({});
+  } catch (error) {
+    error.type = errorTypes.badRequest;
+    error.info = "This is likely to be because of an invalid id";
+    next(error);
+  }
+});
+
 module.exports = router;
